b0caf50e734b0ae7a044346e192cd88b3021b778
syifasitikhumairah <syifasiti158@gmail.com>
2021-12-28 16:32:57 +0700

    Day 4

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        // $user = new User();
+        // $user->name = 'Syifa Siti Khumairah';
+        // $user->email = 'admin@gmail.com';
+        // $user->password = Hash::make('12345678');
+        // $user->save();
+
+        // $user = new User();
+        // $user->name = 'Syifa Siti';
+        // $user->email = 'syifasiti@gmail.com';
+        // $user->password = Hash::make('12345678');
+        // $user->save();
+
+        // $user = new User();
+        // $user->name = 'Syifask';
+        // $user->email = 'syifask@gmail.com';
+        // $user->password = Hash::make('12345678');
+        // $user->save();
+
+        // $user = new User();
+        // $user->name = 'Risma Fadila';
+        // $user->email = 'risma@gmail.com';
+        // $user->password = Hash::make('12345678');
+        // $user->save();
+
+        // $user = new User();
+        // $user->name = 'Ike Vadila';
+        // $user->email = 'ike@gmail.com';
+        // $user->password = Hash::make('12345678');
+        // $user->save();
+
+        $admin = Role::create([
+            'name' => 'admin',
+            'display_name' => 'User Administrator'
+        ]);
+
+        $pengguna = Role::create([
+            'name' => 'pengguna',
+            'display_name' => 'User Biasa'
+        ]);
+
+        $user = new User();
+        $user->name = 'Syifa Siti Khumairah';
+        $user->email = 'admin@gmail.com';
+        $user->password = Hash::make('12345678');
+        $user->save();
+
+        $pengunjung = new User();
+        $pengunjung->name = 'Pengunjung';
+        $pengunjung->email = 'pengunjung@gmail.com';
+        $pengunjung->password = Hash::make('12345678');
+        $pengunjung->save();
+
+        $user->attachRole($admin);
+        $pengunjung->attachRole($pengguna);
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(
+    [
+    'register' => false
+    ]
+);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//hanya untuk role admin
+Route::group(['prefix' => 'admin','middleware' => ['auth','role:admin']], function(){
+    Route::get('/', function(){
+        return 'halaman admin';
+    });
+
+    Route::get('profile', function(){
+        return 'halaman profile admin';
+    });
+});
+
+//hanya untuk role pengguna
+Route::group(['prefix' => 'pengguna','middleware' => ['auth','role:pengguna']], function(){
+    Route::get('/', function(){
+        return 'halaman pengguna';
+    });
+
+    Route::get('profile', function(){
+        return 'halaman profile pengguna';
+    });
+});
