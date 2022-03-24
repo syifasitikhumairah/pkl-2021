cb55d83f1cb5d15a54ee7ae073d7b7f884e8c0c9
syifasitikhumairah <syifasiti158@gmail.com>
2022-03-24 19:33:21 +0700

    update

--- resources/views/frontend/kegiatan.blade.php
+++ resources/views/frontend/kegiatan.blade.php
@@ -7,12 +7,12 @@
         <div class="row row-cols-1 row-cols-md-3 g-4">
             @foreach ($kegiatan as $data)
                 <div class="col">
-                    <div class="card h-100"  style="box-shadow: 0px 0px 12px 0px rgb(34 34 34 / 7%);">
-                        <img src="{{ $data->image() }}" alt="" style="width:324px; height:200px;" alt="Cover"
+                    <div class="card h-100" style="box-shadow: 0px 0px 12px 0px rgb(34 34 34 / 7%);">
+                        <img src="{{ $data->image() }}" alt="" style="width:200px; height:200px;" alt="Cover"
                             class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title" style="padding:5px"><b>{{ $data->judul }}</b></h5>
-                            <p>{!!$data->keterangan!!}</p>
+                            <p>{!! $data->keterangan !!}</p>
                         </div>
                         {{-- <a href="/kegiatan/detail" class="btn btn-outline-primary"
                         style="background:#5cb874;
@@ -20,7 +20,8 @@
                         color: white;
                         font-size: 18px;">Detail</a> --}}
                         <div class="card-footer">
-                           <center><small class="text-muted">{{date('d-m-Y', strtotime($data->tanggal)) }}</small></center>
+                            <center><small class="text-muted">{{ date('d-m-Y', strtotime($data->tanggal)) }}</small>
+                            </center>
                         </div>
 
                     </div>
@@ -30,13 +31,12 @@
         <br>
         <br>
         <div class="form-group text-center">
-            <a href="/kegiatan/all"><button type="submit" class="btn btn-outline-primary"
-            style="background:#5cb874;
-            border-color: #5cb874;
-            color: white;
-            border-radius:25px;
-            width:220px;
-            font-size: 18px;">Lihat Semua Kegiatan</button></a>
+            <a href="/kegiatan/all"><button type="submit" class="btn btn-outline-primary" style="background:#5cb874;
+                    border-color: #5cb874;
+                    color: white;
+                    border-radius:25px;
+                    width:220px;
+                    font-size: 18px;">Lihat Semua Kegiatan</button></a>
         </div>
     </div>
 @endsection
